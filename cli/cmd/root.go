0947250ba08ff68cda648f4713ee2369afb6a794
kyieon <dldudwo87@gmail.com>
2022-08-22 18:17:55 +0900

    add Logger

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -5,9 +5,41 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
+	os "os"
+	"strings"
+	"time"
 )
+
+var Log *log.Logger
+
+func NewLogger() *log.Logger {
+	if Log != nil {
+		return Log
+	}
+
+	path := "./efa.log"
+	writer, _ := rotatelogs.New(
+		path+".%Y%m%d%H",
+		rotatelogs.WithLinkName(path),
+		rotatelogs.WithMaxAge(7*24*time.Hour),
+		rotatelogs.WithRotationTime(time.Hour),
+	)
+
+	Log = log.New()
+	Log.Hooks.Add(lfshook.NewHook(
+		lfshook.WriterMap{
+			log.InfoLevel:  writer,
+			log.ErrorLevel: writer,
+		},
+		&log.JSONFormatter{},
+	))
+	//log.SetOutput(Log.Writer())
+	return Log
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -17,12 +49,14 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	Log.Info("Execute: ", strings.Join(os.Args, " "))
 	err := rootCmd.Execute()
 	if err != nil {
+		Log.Error(err)
 		os.Exit(1)
 	}
 }
 
 func init() {
-
+	NewLogger()
 }
